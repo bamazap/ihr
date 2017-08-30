20debe6fd97a8f97550ed341224f3330a6f75744
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-08-30 12:40:50 -0400

    Fix connection code

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ function handleDisconnect() {
   connection = mysql.createConnection(credentials);
 
   connection.connect(function(err) {
+    if (err) {
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000);
     }
@@ -29,7 +30,7 @@ function handleDisconnect() {
 
   connection.on('error', function(err) {
     console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST')
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
       handleDisconnect();
     } else {
       throw err;
